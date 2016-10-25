ca3acd44a8e9288cab7d5c4bdc868ca25925125d
usuario <usuario@usuario-PC>
2016-10-25 15:20:11 +0200

    añadido clase prueba

---
+++ ejemplo_ED_c/src/prueba.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class prueba {
+    int pp;
+    
+    public prueba() {
+    }
+
+    public int getPp() {
+        return pp;
+    }
+
+    public void setPp(int pp) {
+        this.pp = pp;
+    }
+    
+}
